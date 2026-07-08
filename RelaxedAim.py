c1ecec6985b2c1ecf9275f0ea3d350351dd5dfad
Khoi Pham <qxmagn3tism@gmail.com>
2026-07-08 13:25:23 -0700

    added difficulty scaling - A = easier, D = harder

--- RelaxedAim.py
+++ RelaxedAim.py
@@ -4,6 +4,7 @@ from pygame import mouse
 import random
 import ctypes
 
+#initialize screen
 pygame.init()
 screen_width = 1600
 screen_height = 900
@@ -29,17 +30,20 @@ class Circle:
         circle_center = (self.x, self.y)
         distance = pygame.math.Vector2(circle_center).distance_to(mouse_pos)
         return distance <= self.radius
-circle_radius = 100
-random_x = random.randint(1,screen_width)
-random_y = random.randint(1,screen_height)
-random_2x = random.randint(1,screen_width)
-random_2y = random.randint(1,screen_height)
-random_3x = random.randint(1,screen_width)
-random_3y = random.randint(1,screen_height)
+    def difficulty(self, amount):
+        self.radius += amount
+circle_radius = 30
+circle_circumference = circle_radius * 2
+random_x = random.randint(circle_radius,screen_width)
+random_y = random.randint(circle_radius,screen_height)
+random_2x = random.randint(circle_radius,screen_width)
+random_2y = random.randint(circle_radius,screen_height)
+random_3x = random.randint(circle_radius,screen_width)
+random_3y = random.randint(circle_radius,screen_height)
 circle = Circle(random_x, random_y, circle_radius, red)
 circle2 = Circle(random_2x, random_2y, circle_radius, red)
 circle3 = Circle(random_3x, random_3y, circle_radius, red)
-circle_cursor = Circle(circle.x, circle.y, 10, green)
+circle_cursor = Circle(circle.x, circle.y, 5, green)
 
 
 while running:
@@ -53,19 +57,27 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            if event.key == pygame.K_a:
+                circle.difficulty(5)
+                circle2.difficulty(5)
+                circle3.difficulty(5)
+            if event.key == pygame.K_d:
+                circle.difficulty(-5)
+                circle2.difficulty(-5)
+                circle3.difficulty(-5)
         if circle.detect_mouse(mouse_pos):
-            random_x = random.randint(1, screen_width)
-            random_y = random.randint(1, screen_height)
+            random_x = random.randint(circle_circumference, screen_width - circle_circumference)
+            random_y = random.randint(circle_circumference, screen_height- circle_circumference)
             circle.x = random_x
             circle.y = random_y
         if circle2.detect_mouse(mouse_pos):
-            random2_x = random.randint(1, screen_width)
-            random2_y = random.randint(1, screen_height)
+            random2_x = random.randint(circle_circumference, screen_width - circle_circumference)
+            random2_y = random.randint(circle_circumference, screen_height- circle_circumference)
             circle2.x = random2_x
             circle2.y = random2_y
         if circle3.detect_mouse(mouse_pos):
-            random3_x = random.randint(1, screen_width)
-            random3_y = random.randint(1, screen_height)
+            random3_x = random.randint(circle_circumference, screen_width - circle_circumference)
+            random3_y = random.randint(circle_circumference, screen_height- circle_circumference)
             circle3.x = random3_x
             circle3.y = random3_y
     screen.fill((0, 0, 0))
