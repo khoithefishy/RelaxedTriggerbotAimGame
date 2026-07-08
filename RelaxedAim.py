5fca5f7f01c097aa6b35da0572e0ecaed0f438b1
Khoi Pham <qxmagn3tism@gmail.com>
2026-07-08 14:48:12 -0700

    Added score + difficulty scale + point system + fixed ifs

--- RelaxedAim.py
+++ RelaxedAim.py
@@ -6,6 +6,8 @@ import ctypes
 
 #initialize screen
 pygame.init()
+pygame.font.init()
+pygame.display.set_caption("Relaxed Aim")
 screen_width = 1600
 screen_height = 900
 screen= pygame.display.set_mode((screen_width,screen_height))
@@ -13,9 +15,15 @@ clock= pygame.time.Clock()
 running = True
 red = (255,0,0)
 green = (0,255,0)
+white = (255,255,255)
 mouse_pos = mouse.get_pos()
 pygame.mouse.set_visible(False)
 
+score = 0
+hard = 20
+easy = 70
+# Display font
+font = pygame.font.Font(None, 50)
 
 class Circle:
     def __init__(self, x, y, radius, color):
@@ -32,6 +40,8 @@ class Circle:
         return distance <= self.radius
     def difficulty(self, amount):
         self.radius += amount
+        if self.radius <= 5:
+            self.radius = 5
 circle_radius = 30
 circle_circumference = circle_radius * 2
 random_x = random.randint(circle_radius,screen_width)
@@ -45,12 +55,11 @@ circle2 = Circle(random_2x, random_2y, circle_radius, red)
 circle3 = Circle(random_3x, random_3y, circle_radius, red)
 circle_cursor = Circle(circle.x, circle.y, 5, green)
 
-
+circles = [circle, circle2, circle3]
 while running:
     mouse_pos = mouse.get_pos()
     circle_cursor.x = mouse_pos[0]
     circle_cursor.y = mouse_pos[1]
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running =  False
@@ -58,32 +67,53 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             if event.key == pygame.K_a:
-                circle.difficulty(5)
-                circle2.difficulty(5)
-                circle3.difficulty(5)
+                for circle in circles:
+                    circle.difficulty(5)
             if event.key == pygame.K_d:
-                circle.difficulty(-5)
-                circle2.difficulty(-5)
-                circle3.difficulty(-5)
-        if circle.detect_mouse(mouse_pos):
-            random_x = random.randint(circle_circumference, screen_width - circle_circumference)
-            random_y = random.randint(circle_circumference, screen_height- circle_circumference)
-            circle.x = random_x
-            circle.y = random_y
-        if circle2.detect_mouse(mouse_pos):
-            random2_x = random.randint(circle_circumference, screen_width - circle_circumference)
-            random2_y = random.randint(circle_circumference, screen_height- circle_circumference)
-            circle2.x = random2_x
-            circle2.y = random2_y
-        if circle3.detect_mouse(mouse_pos):
-            random3_x = random.randint(circle_circumference, screen_width - circle_circumference)
-            random3_y = random.randint(circle_circumference, screen_height- circle_circumference)
-            circle3.x = random3_x
-            circle3.y = random3_y
+                for circle in circles:
+                    circle.difficulty(-5)
+        for circle in circles:
+            if circle.detect_mouse(mouse_pos):
+                random_x = random.randint(circle_circumference, screen_width - circle_circumference)
+                random_y = random.randint(circle_circumference, screen_height- circle_circumference)
+                circle.x = random_x
+                circle.y = random_y
+                if circle.radius <= hard:
+                    score += 200
+                elif circle.radius >= easy:
+                    score += 50
+                else:
+                    score += 100
+        # if circle.detect_mouse(mouse_pos):
+        #     random_x = random.randint(circle_circumference, screen_width - circle_circumference)
+        #     random_y = random.randint(circle_circumference, screen_height- circle_circumference)
+        #     circle.x = random_x
+        #     circle.y = random_y
+        #     if circle.radius <= hard:
+        #         score += 200
+        #     elif circle.radius >= easy:
+        #         score += 50
+        #     else:
+        #         score += 100
+        # if circle2.detect_mouse(mouse_pos):
+        #     random2_x = random.randint(circle_circumference, screen_width - circle_circumference)
+        #     random2_y = random.randint(circle_circumference, screen_height- circle_circumference)
+        #     circle2.x = random2_x
+        #     circle2.y = random2_y
+        #     score += 100
+        # if circle3.detect_mouse(mouse_pos):
+        #     random3_x = random.randint(circle_circumference, screen_width - circle_circumference)
+        #     random3_y = random.randint(circle_circumference, screen_height- circle_circumference)
+        #     circle3.x = random3_x
+        #     circle3.y = random3_y
+        #     score += 100
     screen.fill((0, 0, 0))
-    circle.draw()
-    circle2.draw()
-    circle3.draw()
+    for circle in circles:
+        circle.draw()
     circle_cursor.draw()
+    text = font.render(f'Score: {score}', True, white)
+    text_circle_size = font.render(f'Size: {circle.radius}', True, white)
+    screen.blit(text, (50,30))
+    screen.blit(text_circle_size, (50, 60))
     pygame.display.update()
     clock.tick(360)
