8e9a7359712a189df7620ee3910bfcf04127139e
Khoi Pham <qxmagn3tism@gmail.com>
2026-07-09 17:18:33 -0700

    Brushed up + options to add more circles

--- RelaxedAim.py
+++ RelaxedAim.py
@@ -4,18 +4,18 @@ from pygame import mouse
 import random
 import ctypes
 
-#initialize screen
+# initialize screen
 pygame.init()
 pygame.font.init()
 pygame.display.set_caption("Relaxed Aim")
 screen_width = 1600
 screen_height = 900
-screen= pygame.display.set_mode((screen_width,screen_height))
-clock= pygame.time.Clock()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
 running = True
-red = (255,0,0)
-green = (0,255,0)
-white = (255,255,255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
 mouse_pos = mouse.get_pos()
 pygame.mouse.set_visible(False)
 hit_sfx = pygame.mixer.Sound('hitsfx.WAV')
@@ -23,50 +23,58 @@ score = 0
 hard = 20
 easy = 70
 # Display font
-font = pygame.font.Font(None, 50)
+font = pygame.font.Font(None, 30)
 
 
+# Create Circle Class
 class Circle:
     def __init__(self, x, y, radius, color):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
+
+    # Function --> Draws, detect the mouse, size, sfx
     def draw(self):
-        pygame.draw.circle(screen, (self.color),(self.x, self.y),self.radius)
-    #Check if cursor is within circle
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+    # Check if cursor is within circle
     def detect_mouse(self, mouse_pos):
         circle_center = (self.x, self.y)
         distance = pygame.math.Vector2(circle_center).distance_to(mouse_pos)
         return distance <= self.radius
+
     def difficulty(self, amount):
         self.radius += amount
         if self.radius <= 5:
             self.radius = 5
+
     def sfx(self):
         pygame.mixer.Sound.play(hit_sfx)
 
+
 circle_radius = 30
 circle_circumference = circle_radius * 2
-random_x = random.randint(circle_radius,screen_width)
-random_y = random.randint(circle_radius,screen_height)
-random_2x = random.randint(circle_radius,screen_width)
-random_2y = random.randint(circle_radius,screen_height)
-random_3x = random.randint(circle_radius,screen_width)
-random_3y = random.randint(circle_radius,screen_height)
+random_x = random.randint(circle_radius, screen_width)
+random_y = random.randint(circle_radius, screen_height)
 circle = Circle(random_x, random_y, circle_radius, red)
-circle2 = Circle(random_2x, random_2y, circle_radius, red)
-circle3 = Circle(random_3x, random_3y, circle_radius, red)
-circle_cursor = Circle(circle.x, circle.y, 5, green)
+circle_cursor = Circle(circle.x, circle.y, 10, green)
+circles = []
 
-circles = [circle, circle2, circle3]
+for i in range(3):
+    random_x = random.randint(circle_radius, screen_width)
+    random_y = random.randint(circle_radius, screen_height)
+    circles.append(Circle(random_x, random_y, circle_radius, red))
+
 while running:
     mouse_pos = mouse.get_pos()
     circle_cursor.x = mouse_pos[0]
     circle_cursor.y = mouse_pos[1]
+    random_x = random.randint(circle_circumference, screen_width - circle_circumference)
+    random_y = random.randint(circle_circumference, screen_height - circle_circumference)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running =  False
+            running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
@@ -76,10 +84,12 @@ while running:
             if event.key == pygame.K_d:
                 for circle in circles:
                     circle.difficulty(-5)
+            if event.key == pygame.K_1:
+                circles.append(Circle(random_x, random_y, circle.radius, red))
+            if event.key == pygame.K_2:
+                circles.remove(circle)
         for circle in circles:
             if circle.detect_mouse(mouse_pos):
-                random_x = random.randint(circle_circumference, screen_width - circle_circumference)
-                random_y = random.randint(circle_circumference, screen_height- circle_circumference)
                 circle.x = random_x
                 circle.y = random_y
                 circle.sfx()
@@ -89,36 +99,16 @@ while running:
                     score += 50
                 else:
                     score += 100
-        # if circle.detect_mouse(mouse_pos):
-        #     random_x = random.randint(circle_circumference, screen_width - circle_circumference)
-        #     random_y = random.randint(circle_circumference, screen_height- circle_circumference)
-        #     circle.x = random_x
-        #     circle.y = random_y
-        #     if circle.radius <= hard:
-        #         score += 200
-        #     elif circle.radius >= easy:
-        #         score += 50
-        #     else:
-        #         score += 100
-        # if circle2.detect_mouse(mouse_pos):
-        #     random2_x = random.randint(circle_circumference, screen_width - circle_circumference)
-        #     random2_y = random.randint(circle_circumference, screen_height- circle_circumference)
-        #     circle2.x = random2_x
-        #     circle2.y = random2_y
-        #     score += 100
-        # if circle3.detect_mouse(mouse_pos):
-        #     random3_x = random.randint(circle_circumference, screen_width - circle_circumference)
-        #     random3_y = random.randint(circle_circumference, screen_height- circle_circumference)
-        #     circle3.x = random3_x
-        #     circle3.y = random3_y
-        #     score += 100
+
     screen.fill((0, 0, 0))
     for circle in circles:
         circle.draw()
     circle_cursor.draw()
     text = font.render(f'Score: {score}', True, white)
     text_circle_size = font.render(f'Size: {circle.radius}', True, white)
-    screen.blit(text, (50,30))
+    text_difficulty = font.render('Press 1,2 to increase/decrease circles.', True, white)
+    screen.blit(text, (50, 30))
     screen.blit(text_circle_size, (50, 60))
+    screen.blit(text_difficulty, (50, 90))
     pygame.display.update()
     clock.tick(360)
